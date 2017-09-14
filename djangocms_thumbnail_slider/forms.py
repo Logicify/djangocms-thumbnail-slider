19d22c7d8fe176da62b1f361717e077a658378af
DmitriySelischev <dmitriy.selischev@logicify.com>
2017-09-14 17:44:39 +0300

    Added form and development tools

--- djangocms_thumbnail_slider/forms.py
+++ djangocms_thumbnail_slider/forms.py
@@ -1,6 +1,17 @@
 from django import forms
+from django.utils.translation import ugettext_lazy as _
+from djangocms_thumbnail_slider import models
 
 
 # Create your forms here
 class ThumbnailSlideForm(forms.ModelForm):
-    pass
+    class Meta:
+        model = models.ThumbnailSlide
+        fields = '__all__'
+
+    def clean(self):
+        data = super(ThumbnailSlideForm, self).clean()
+        if data['image'] is None and not data['link']:
+            self.add_error('image', _('Image or link should be provided'))
+            self.add_error('link', _('Image or link should be provided'))
+        return data
